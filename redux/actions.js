351c30a71be18c908b07dce7430c49d70c631e44
Bryce Steinhoff <bryce@brycesteinhoff.com>
2016-02-26 21:07:37 -0600

    Work in progress

--- redux/actions.js
+++ redux/actions.js
@@ -2,13 +2,6 @@
 
 import { push as routerPush } from 'react-router-redux';
 
-// Example
-export function clearResults() {
-    return {
-        type: 'CLEAR_RESULTS'
-    };
-};
-
 // Change route
 export function changeRoute(route) {
     return (dispatch, getState) => {
